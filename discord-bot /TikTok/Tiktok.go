1c07bec7c867707346fffb119724897650610e03
Horse1jj <foste1jj.seth@gmail.com>
2024-12-05 16:04:53 -0500

    Update Tiktok.go

--- discord-bot /TikTok/Tiktok.go
+++ discord-bot /TikTok/Tiktok.go
@@ -1,1 +1,24 @@
+package TikTok
+
+import (
+	"log"
+	"time"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+func MonitorTikTok(s *discordgo.Session, config map[string]interface{}) {
+	tiktokUsers := config["TikTokUsers"].([]string)
+	discordChannelID := config["DiscordChannelID"].(string)
+
+	for {
+		for _, user := range tiktokUsers {
+			// Fetch TikTok data (scraping or third-party APIs)
+			log.Printf("Checking TikTok updates for %s...", user)
+			// Notify in Discord if there's a new video
+		}
+
+		time.Sleep(10 * time.Minute)
+	}
+}
 
