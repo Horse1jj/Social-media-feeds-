c480850f96c3e78056b9907279581372a6009090
Horse1jj <foste1jj.seth@gmail.com>
2024-12-05 16:04:18 -0500

    Update main.go

--- discord-bot /main.go
+++ discord-bot /main.go
@@ -1,1 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/bwmarrin/discordgo"
+	"discord-bot/YouTube"
+	"discord-bot/TikTok"
+	"discord-bot/Twitch"
+	"discord-bot/Commands"
+)
+
+func main() {
+	// Load configuration
+	config := loadConfig("config.json")
+
+	// Initialize Discord bot
+	dg, err := discordgo.New("Bot " + config.BotToken)
+	if err != nil {
+		log.Fatalf("Error creating Discord session: %v", err)
+	}
+
+	// Add command handlers
+	dg.AddMessageCreateHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
+		Commands.HandleCommands(s, m)
+	})
+
+	// Start social media monitors
+	go YouTube.MonitorYouTube(dg, config)
+	go TikTok.MonitorTikTok(dg, config)
+	go Twitch.MonitorTwitch(dg, config)
+
+	// Open WebSocket connection
+	err = dg.Open()
+	if err != nil {
+		log.Fatalf("Error opening WebSocket connection: %v", err)
+	}
+	defer dg.Close()
+
+	fmt.Println("Bot is running. Press CTRL+C to exit.")
+	select {}
+}
 
